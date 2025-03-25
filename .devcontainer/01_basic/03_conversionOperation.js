acd56e234ca8e7b2d390173ea84e8aa35d1b923b
LSR9966 <166851457+LSR9966@users.noreply.github.com>
2025-03-25 10:27:16 +0000

    CONVERSION OF DATATYPES

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score=33
+console.log(score)
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber= Number(score);
+console.log(typeof(valueinNumber))
+console.log(valueinNumber);
+
+//"33" is easyily converted can also give NaN Not a Number
+//"33abc" =>Nan
+//true=> 1; false=>0    
+
+let islogged="ha"
+let booleanISloggedIn=Boolean(islogged)
+console.log(booleanISloggedIn)
+
+// 1 => true
+// 0=> false
+// empty string =>false
+// normal string => true
+
