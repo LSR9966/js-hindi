f2a086f6cf8ed348402a5c83fceefa41903efe6d
LSR9966 <166851457+LSR9966@users.noreply.github.com>
2025-03-25 10:06:38 +0000

    datatypes

---
+++ .devcontainer/01_basic/01_var.js
@@ -0,0 +1,16 @@
+const accountId=14453
+let accountEmail='lalit21@google.com'
+var accountPass="12345"
+accountCity='jaipur'
+let accountState;
+
+//accountId=2;
+/*
+prefer not to use var because if issue in block scope and functional scope
+*/
+
+accountEmail="herrhai";
+accountPass="45667";
+accountCity="delhi";
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) we are using node js,not browser
+
+//code readability is very high
+
+//ECMA SCRIPT STANDARD THE JAVASCRIPT...
+
+let name="lalit"
+let age=32
+let isRaining=true
+let state;
+// PRIMITIVE DATATYPE
+
+// bigint is used when the number is too large
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined
+// symbol=> unique
+// null ka type hai object
+// undefined ka type hai undefined
+
+console.log(typeof(state));
+console.log(typeof(name));
+console.log(typeof undefined)
+
