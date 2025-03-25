7c07559139c2ec8b8a6378c6463af4afc88a7fcf
LSR9966 <166851457+LSR9966@users.noreply.github.com>
2025-03-25 10:54:27 +0000

    Comparison

--- .devcontainer/01_basic/03_conversionOperation.js
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -15,8 +15,30 @@ let islogged="ha"
 let booleanISloggedIn=Boolean(islogged)
 console.log(booleanISloggedIn)
 
+console.log()
+
 // 1 => true
 // 0=> false
 // empty string =>false
 // normal string => true
 
+
+//**************OPERATIONS **********************//
+
+let a=3
+let b=-a
+console.log(b)
+console.log(4**3) // square kar dega
+
+
+let str1="lalit"
+let str2="singh"
+console.log(str1+str2)  //concatenation
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2+2)
+console.log(1+2+2+"2")
+console.log(+true)//1
+console.log(-"")//0
---
+++ .devcontainer/01_basic/04_comparison.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+console.log(3>=2);
+console.log(3==3);
+console.log();
+
+
+console.log("2" < 1);//automatically converted string into number
+console.log("4" <=4);
+console.log()
+
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+
+// === triple equal check the number as well as the datatype
+
+console.log("2"===2);
+
+
+
+
+
+
