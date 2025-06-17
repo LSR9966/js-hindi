1eeaaed93602be24c0c5b472f33424e825b3dd1e
LSR9966 <166851457+LSR9966@users.noreply.github.com>
2025-06-17 00:49:41 +0000

    WHILE_loop

---
+++ .devcontainer/01_basic/05_iterations/02_two.js
@@ -0,0 +1,22 @@
+let index=0
+while(index<=10)
+{
+    console.log(index);
+    index++;  
+}
+
+let myarray=["flash",'batman','superman'];
+let arr=0;
+
+while(arr<myarray.length)
+{
+    console.log(myarray[arr]);
+    arr++;
+}
+
+let score=1
+do{
+    console.log(score);
+    score++;
+}
+while(score<5)
